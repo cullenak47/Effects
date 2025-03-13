c5ad0af19143289e33d0a311a2992a537de1c7d0
cullenak47 <44653922+cullenak47@users.noreply.github.com>
2025-03-13 18:19:10 +0000

    Update TwoStageFFTConvolverParallel.cpp

--- src/TwoStageFFTConvolverParallel.cpp
+++ src/TwoStageFFTConvolverParallel.cpp
@@ -19,7 +19,7 @@
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // ==================================================================================
 
-#include "/home/Cullen/FYP/Effects/Effects/include/TwoStageFFTConvolverParallel.h"
+#include "/home/Cullen/FYP/Effects/Effects/include/TwoStageConvolverParallel.h"
 
 #include <algorithm>
 #include <cmath>
